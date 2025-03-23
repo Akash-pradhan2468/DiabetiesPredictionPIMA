4f39f61f96a60db7ac7b8ba7a246e6ab852eee58
Akash-pradhan2468 <akash2468pradhan@gmail.com>
2025-03-23 21:09:32 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import numpy as np
 from flask import Flask,request,render_template
 import pickle
 from sklearn.preprocessing import StandardScaler
-flask_app=Flask(__name__)
+app=Flask(__name__)
 model=pickle.load(open("model.pkl",'rb'))
 
 scaler = pickle.load(open("scaler.pkl", 'rb'))
 
-@flask_app.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@flask_app.route("/predict",methods=["POST"])
+@app.route("/predict",methods=["POST"])
 def predict():
     float_features=[float(x) for x in request.form.values()]
     features=np.array(float_features)
@@ -26,4 +26,4 @@ def predict():
     return render_template("index.html",prediction_text="{}".format(ans))
 
 if __name__=="__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
